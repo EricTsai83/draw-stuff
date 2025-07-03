2130b312b8f928585de54ca9f4b88f8e75fdcd41
Eric Tsai <eric492718@gmail.com>
2025-07-04 01:05:55 +0800

    setup uploadthing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import ExcalidrawClientWrapper from "@/components/excalidraw/excalidraw-client-wrapper";
 
 export default function Home() {
-  return (
-    <div className="">
-      <ExcalidrawClientWrapper />
-    </div>
-  );
+  return <ExcalidrawClientWrapper />;
 }
